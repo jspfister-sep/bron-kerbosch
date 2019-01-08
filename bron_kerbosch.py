e409aa92b4774db1c8c4646547d96e16133a1cc7
= <jspfister@sep.com>
2019-01-08 00:16:40 -0500

    Initial Bron-Kerbosch algorithms with example

---
+++ bron_kerbosch.py
@@ -0,0 +1,36 @@
+import sys
+
+def bron_kerbosch(r, p, x, maximal_cliques=[], level=0):
+    print(f'{"  " * level}R={r}, P={p}, X={x}{" <--" if not p and not x else ""}')
+    if not p and not x:
+        maximal_cliques.append(r)
+    else:
+        for v in p.copy():
+            bron_kerbosch(r | {v}, p & neighbors[v], x & neighbors[v], maximal_cliques, level + 1)
+            p.remove(v)
+            x.add(v)
+
+def bron_kerbosch_with_pivot(r, p, x, maximal_cliques=[], level=0):
+    print(f'{"  " * level}R={r}, P={p}, X={x}{" <--" if not p and not x else ""}')
+    if not p and not x:
+        maximal_cliques.append(r)
+    else:
+        u = max(p | x, key=lambda s: len(neighbors[s]))
+        for v in p - neighbors[u]:
+            bron_kerbosch_with_pivot(r | {v}, p & neighbors[v], x & neighbors[v], maximal_cliques, level + 1)
+            p.remove(v)
+            x.add(v)
+
+if __name__ == '__main__':
+    neighbors = {
+        1 : {2, 5},
+        2 : {1, 3, 5},
+        3 : {2, 4},
+        4 : {3, 5, 6},
+        5 : {1, 2, 4},
+        6 : {4}}
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-p':
+        bron_kerbosch_with_pivot(set(), {1, 2, 3, 4, 5, 6}, set())
+    else:
+        bron_kerbosch(set(), {1, 2, 3, 4, 5, 6}, set())
